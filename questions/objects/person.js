65f9b1ec4653a75b55198a8a76b598d303803462
Rex Posadas <rposadas@redwoodlogistics.com>
2022-07-10 16:47:51 +0800

    added cashflow assignment

---
+++ questions/objects/person.js
@@ -0,0 +1,14 @@
+
+// GOAL: 
+// console.log should print the following message;
+// 
+// name of person is james reyes
+// he is 33 years old
+
+
+// TODO: Define the object so that we get the desired results.
+let person = {
+}
+    
+console.log("name of person is " + person.name + " "+ person.lastname)
+console.log("he is " + person.age, "years old")
