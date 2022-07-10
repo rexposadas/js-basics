e6f55daf04600107b856c156afec4c22edcc9ccd
Rex Posadas <rposadas@redwoodlogistics.com>
2022-07-10 16:49:55 +0800

    cashflow

---
+++ questions/objects/cash_flow.js
@@ -0,0 +1,19 @@
+// Goal:
+// console.log(...) should print the following line:
+
+// After all the account activities, your account balance is: 4300 Pesos
+
+function getbalance(cashflow){
+    // TODO: Write the code that belongs here.
+}
+    
+let cash_flow = {
+    earnings: 10000,
+    misc_earnings: 4500, 
+    car : -3000,
+    shopping: -200, 
+    rent: -7000
+}
+    
+
+// TODO: Write a console.log statment that will give us the results.
